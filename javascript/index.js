d017a3f6405147a071569b75dc10e6f11915ebd4
Cristian Antonescu <zizou.unique927@gmail.com>
2018-10-16 04:30:28 +0300

    Survey about the web (only front-end part done)

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+
+function otherFunction() {
+    var x = document.getElementById("textarea-for-other");
+    if (x.style.display = "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function funcSocialMedia() {
+    var z = document.getElementById('textarea-for-social-media');
+    if (z.style.display = 'none') {
+        z.style.display = 'block';
+    } else {
+        z.style.display = 'none';
+    }
+}
